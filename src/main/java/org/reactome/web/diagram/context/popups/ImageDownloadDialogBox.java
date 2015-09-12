6e02d2ab90cd611a7350ce376a85b6117461d909
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2015-09-12 15:51:48 +0100

    minor change

---
+++ src/main/java/org/reactome/web/diagram/context/popups/ImageDownloadDialogBox.java
@@ -0,0 +1,10 @@
+package org.reactome.web.diagram.context.popups;
+
+import com.google.gwt.user.client.ui.DialogBox;
+
+/**
+ * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
+ */
+public class ImageDownloadDialogBox  extends DialogBox{
+
+}
