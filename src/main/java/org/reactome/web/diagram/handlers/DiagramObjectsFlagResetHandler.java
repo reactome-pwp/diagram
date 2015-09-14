80d2ee8d8a6dc5197488a696a24aea85e2091e9d
Antonio Fabregat <fabregat.antonio@gmail.com>
2015-09-14 14:30:23 +0100

    event passed to the flag reset handler

--- src/main/java/org/reactome/web/diagram/handlers/DiagramObjectsFlagResetHandler.java
+++ src/main/java/org/reactome/web/diagram/handlers/DiagramObjectsFlagResetHandler.java
@@ -1,10 +1,11 @@
 package org.reactome.web.diagram.handlers;
 
 import com.google.gwt.event.shared.EventHandler;
+import org.reactome.web.diagram.events.DiagramObjectsFlagResetEvent;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 public interface DiagramObjectsFlagResetHandler extends EventHandler {
-    void onDiagramObjectsFlagReset();
+    void onDiagramObjectsFlagReset(DiagramObjectsFlagResetEvent event);
 }
