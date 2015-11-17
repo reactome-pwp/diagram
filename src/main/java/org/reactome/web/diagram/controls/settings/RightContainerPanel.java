71c0e2760b341092b493c9e02cdf3f64807b5e63
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2015-11-17 14:21:48 +0000

    code clean up

--- src/main/java/org/reactome/web/diagram/controls/settings/RightContainerPanel.java
+++ src/main/java/org/reactome/web/diagram/controls/settings/RightContainerPanel.java
@@ -1,7 +1,6 @@
 package org.reactome.web.diagram.controls.settings;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -13,15 +12,6 @@ public class RightContainerPanel extends FlowPanel {
 
     public RightContainerPanel() {
         this.setStyleName(RESOURCES.getCSS().container());
-
-
-        FlowPanel fp = new FlowPanel();
-        fp.getElement().getStyle().setHeight(200, Style.Unit.PX);
-        fp.getElement().getStyle().setWidth(50, Style.Unit.PX);
-        fp.getElement().getStyle().setBackgroundColor("#00FF00");
-        fp.getElement().getStyle().setFloat(Style.Float.LEFT);
-        fp.getElement().getStyle().setMarginRight(5, Style.Unit.PX);
-//        this.add(fp);
     }
 
     public static Resources RESOURCES;
