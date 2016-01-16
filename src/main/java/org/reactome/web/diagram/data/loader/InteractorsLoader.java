d4e122ba90bd9cc92e7e552c72a325a527d88773
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-01-16 14:25:56 +0000

    still loading from file

--- src/main/java/org/reactome/web/diagram/data/loader/InteractorsLoader.java
+++ src/main/java/org/reactome/web/diagram/data/loader/InteractorsLoader.java
@@ -2,16 +2,9 @@ package org.reactome.web.diagram.data.loader;
 
 import com.google.gwt.http.client.*;
 import org.reactome.web.diagram.client.DiagramFactory;
-import org.reactome.web.diagram.data.DiagramContent;
-import org.reactome.web.diagram.data.graph.model.GraphPhysicalEntity;
 import org.reactome.web.diagram.data.interactors.raw.RawInteractors;
 import org.reactome.web.diagram.data.interactors.raw.factory.InteractorsException;
 import org.reactome.web.diagram.data.interactors.raw.factory.InteractorsFactory;
-import org.reactome.web.diagram.data.layout.DiagramObject;
-import org.reactome.web.diagram.data.layout.Node;
-import org.springframework.util.StringUtils;
-
-import java.util.*;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
@@ -40,29 +33,16 @@ public class InteractorsLoader implements RequestCallback {
         }
     }
 
-    public void load(DiagramContent content, String resource){
+    public void load(String stId, String resource){
         if(resource==null){
             this.handler.onInteractorsLoaderError(new InteractorsException(resource, "Resource not specified"));
             return;
         }
         this.resource = resource;
-
-        StringBuilder post = new StringBuilder();
-        for (DiagramObject diagramObject : content.getDiagramObjects()) {
-            if(diagramObject instanceof Node){
-                GraphPhysicalEntity pe = ((Node) diagramObject).getGraphObject();
-                if(pe.getIdentifier()!=null) {
-                    post.append(pe.getIdentifier()).append(",");
-                }
-            }
-        }
-        if(post.length()>0) post.delete(post.length()-1, post.length());
-
-        String url = PREFIX + "static/proteins/details/?v=" + LoaderManager.version;
-        RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.POST, url);
+        String url = PREFIX + resource + "/" + stId + ".details.json?v=" + LoaderManager.version;
+        RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, url);
         try {
-
-            this.request = requestBuilder.sendRequest(post.toString(), this);
+            this.request = requestBuilder.sendRequest(null, this);
         } catch (RequestException e) {
             this.handler.onInteractorsLoaderError(new InteractorsException(resource, e.getMessage()));
         }
