5269b927c722ad69d381fbd1ae7bea885625e074
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-01-19 22:58:34 +0000

    First implementation of the info panel for interactors

---
+++ src/main/java/org/reactome/web/diagram/search/infopanel/InteractorInfoPanel.java
@@ -0,0 +1,54 @@
+package org.reactome.web.diagram.search.infopanel;
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.ui.*;
+import org.reactome.web.diagram.data.interactors.model.InteractorSearchResult;
+
+
+/**
+ * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
+ */
+public class InteractorInfoPanel extends Composite {
+
+    InteractorSearchResult interactor;
+    EventBus eventBus;
+    FlowPanel mainPanel;
+
+    public InteractorInfoPanel(EventBus eventBus, InteractorSearchResult interactor) {
+        this.eventBus = eventBus;
+        this.interactor = interactor;
+        this.mainPanel = new FlowPanel();
+
+        GraphObjectInfoPanel.SuggestionPanelCSS css = GraphObjectInfoPanel.OBJECT_INFO_RESOURCES.getCSS();
+        this.init(css);
+
+        FlowPanel header = new FlowPanel();
+        header.setStyleName(css.infoHeader());
+        header.add(new InlineLabel(interactor.getAcc()));
+        this.add(header);
+        this.add(new Label("Type: Interactor"));
+        //TODO need access to the context
+    }
+
+    /**
+     * Padding grows wider the div but this is not the expected behaviour here. To make it compatible
+     * across all browsers the recommendation is having two divs
+     * <div> // defines the object info panel properties
+     *     <div> // defines the object info content (set the padding here)
+     *         Info content
+     *     </div>
+     * </div>
+     */
+    private void init(GraphObjectInfoPanel.SuggestionPanelCSS css){
+        SimplePanel sp = new SimplePanel();
+        sp.setStyleName(css.objectInfoPanel());
+        this.mainPanel.setStyleName(css.objectInfoContent());
+        sp.add(this.mainPanel);
+        initWidget(sp);
+    }
+
+    public void add(IsWidget widget){
+        this.mainPanel.add(widget);
+    }
+
+}
