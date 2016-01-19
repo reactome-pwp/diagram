1a519e07818b8d6b7f877275b00b5d3115fd36cc
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-01-19 22:59:44 +0000

    minor change

--- src/main/java/org/reactome/web/diagram/search/infopanel/SelectionInfoPanel.java
+++ src/main/java/org/reactome/web/diagram/search/infopanel/SelectionInfoPanel.java
@@ -2,6 +2,7 @@ package org.reactome.web.diagram.search.infopanel;
 
 import com.google.gwt.event.shared.EventBus;
 import org.reactome.web.diagram.data.graph.model.GraphObject;
+import org.reactome.web.diagram.data.interactors.model.InteractorSearchResult;
 import org.reactome.web.diagram.search.SearchResultObject;
 import org.reactome.web.diagram.search.events.SuggestionSelectedEvent;
 import org.reactome.web.diagram.search.handlers.SuggestionSelectedHandler;
@@ -25,8 +26,9 @@ public class SelectionInfoPanel extends AbstractAccordionPanel implements Sugges
         if(obj!=null) {
             if(obj instanceof GraphObject) {
                 this.add(new GraphObjectInfoPanel(eventBus, (GraphObject) obj));
+            } else if(obj instanceof InteractorSearchResult) {
+                this.add(new InteractorInfoPanel(eventBus, (InteractorSearchResult) obj));
             }
-            //TODO
         }
     }
 }
