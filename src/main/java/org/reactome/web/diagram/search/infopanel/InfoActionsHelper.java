d174125cc57a86c2f5fd6e272d0be1c6c5c20799
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-02-03 14:43:04 +0000

    New feature: The search results now include information about the interactors and get updated on any resource change

--- src/main/java/org/reactome/web/diagram/search/infopanel/InfoActionsHelper.java
+++ src/main/java/org/reactome/web/diagram/search/infopanel/InfoActionsHelper.java
@@ -3,8 +3,10 @@ package org.reactome.web.diagram.search.infopanel;
 import com.google.gwt.event.dom.client.*;
 import com.google.gwt.event.shared.EventBus;
 import org.reactome.web.diagram.data.graph.model.GraphObject;
+import org.reactome.web.diagram.data.interactors.raw.RawInteractor;
 import org.reactome.web.diagram.events.GraphObjectHoveredEvent;
 import org.reactome.web.diagram.events.GraphObjectSelectedEvent;
+import org.reactome.web.diagram.events.InteractorSelectedEvent;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
@@ -37,4 +39,44 @@ abstract class InfoActionsHelper {
             }
         };
     }
+
+    /////////////////
+    // Interactors //
+    /////////////////
+
+    static ClickHandler getInteractorLinkClickHandler(final RawInteractor rawInteractor, final EventBus eventBus, final Object source){
+        return new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                eventBus.fireEventFromSource(new InteractorSelectedEvent(rawInteractor, InteractorSelectedEvent.ObjectType.INTERACTOR), source);
+            }
+        };
+    }
+
+    static ClickHandler getInteractionLinkClickHandler(final RawInteractor rawInteractor, final EventBus eventBus, final Object source){
+        return new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                eventBus.fireEventFromSource(new InteractorSelectedEvent(rawInteractor, InteractorSelectedEvent.ObjectType.INTERACTION), source);
+            }
+        };
+    }
+
+//    static MouseOverHandler getInteractorLinkMouseOver(final DiagramInteractor diagramInteractor, final EventBus eventBus, final Object source){
+//        return new MouseOverHandler() {
+//            @Override
+//            public void onMouseOver(MouseOverEvent event) {
+//                eventBus.fireEventFromSource(new InteractorHoveredEvent(diagramInteractor), source);
+//            }
+//        };
+//    }
+//
+//    static MouseOutHandler getInteractorLinkMouseOut(final EventBus eventBus, final Object source){
+//        return new MouseOutHandler() {
+//            @Override
+//            public void onMouseOut(MouseOutEvent event) {
+//                eventBus.fireEventFromSource(new InteractorHoveredEvent(null), source);
+//            }
+//        };
+//    }
 }
