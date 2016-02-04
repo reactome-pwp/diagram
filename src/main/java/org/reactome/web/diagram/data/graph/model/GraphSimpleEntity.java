4fba5aaf1af783f88caad51a3f68c5345eaf3110
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-02-04 16:23:34 +0000

    diagram entities structures (images) infractrusture

--- src/main/java/org/reactome/web/diagram/data/graph/model/GraphSimpleEntity.java
+++ src/main/java/org/reactome/web/diagram/data/graph/model/GraphSimpleEntity.java
@@ -1,13 +1,18 @@
 package org.reactome.web.diagram.data.graph.model;
 
+import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.ui.Image;
 import org.reactome.web.diagram.data.graph.model.images.GraphObjectImages;
 import org.reactome.web.diagram.data.graph.raw.EntityNode;
+import org.reactome.web.diagram.util.chemical.ChEBI_ImageLoader;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
-public class GraphSimpleEntity extends GraphPhysicalEntity {
+public class GraphSimpleEntity extends GraphPhysicalEntity implements ChEBI_ImageLoader.Handler {
+
+    private ImageElement chemicalImage;
 
     public GraphSimpleEntity(EntityNode node) {
         super(node);
@@ -16,5 +21,18 @@ public class GraphSimpleEntity extends GraphPhysicalEntity {
     @Override
     public ImageResource getImageResource() {
         return GraphObjectImages.INSTANCE.simpleEntity();
+    }
+
+    public ImageElement getChemicalImage() {
+        if(chemicalImage == null){
+            chemicalImage = ImageElement.as(ChEBI_ImageLoader.LOADING.getElement());
+            ChEBI_ImageLoader.get().loadImage(this, getIdentifier());
+        }
+        return chemicalImage;
+    }
+
+    @Override
+    public void onChemicalImageLoaded(Image image) {
+        chemicalImage = ImageElement.as(image.getElement());
     }
 }
