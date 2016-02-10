05d7712126fc14636be9f134757b2c75305cf800
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-02-10 11:16:41 +0000

    Interactor entities are progressively fading in and out

--- src/main/java/org/reactome/web/diagram/renderers/interactor/s050/StaticLinkRenderer050.java
+++ src/main/java/org/reactome/web/diagram/renderers/interactor/s050/StaticLinkRenderer050.java
@@ -1,9 +1,19 @@
 package org.reactome.web.diagram.renderers.interactor.s050;
 
+import org.reactome.web.diagram.data.interactors.model.DiagramInteractor;
+import org.reactome.web.diagram.data.layout.Coordinate;
 import org.reactome.web.diagram.renderers.interactor.abs.StaticLinkAbstractRenderer;
+import org.reactome.web.diagram.util.AdvancedContext2d;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 public class StaticLinkRenderer050 extends StaticLinkAbstractRenderer {
+    @Override
+    public void draw(AdvancedContext2d ctx, DiagramInteractor item, Double factor, Coordinate offset) {
+        ctx.save();
+        ctx.setGlobalAlpha((factor - 0.5) * 2);
+        super.draw(ctx, item, factor, offset);
+        ctx.restore();
+    }
 }
