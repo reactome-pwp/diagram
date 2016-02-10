b4755aea1ad22434789a62dd3567c3085c4703f0
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-02-10 15:43:49 +0000

    crossorigin property set in the img tag to force the Access-Control-Allow-Origin header

--- src/main/java/org/reactome/web/diagram/util/chemical/Chemical_ImageLoader.java
+++ src/main/java/org/reactome/web/diagram/util/chemical/Chemical_ImageLoader.java
@@ -6,7 +6,6 @@ import com.google.gwt.event.dom.client.ErrorHandler;
 import com.google.gwt.event.dom.client.LoadEvent;
 import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.event.shared.EventBus;
-import com.google.gwt.http.client.*;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.Image;
@@ -66,44 +65,23 @@ public class Chemical_ImageLoader {
         String id = identifier.replaceAll("^CHEMBL[-:_]?", "");
         String url = "http://www.ebi.ac.uk/chembl/api/data/image/" + id + "?format=png";
 //        String url = "https://www.ebi.ac.uk/chembl/api/data/image/CHEMBL209793?format=png";
-
-        //The following work forces a Access-Control-Allow-Origin header by CHEMBL because
-        //it is only added when the header X-Requested-With:XMLHttpRequest is added
-        //Ideally the Access-Control-Allow-Origin:* should be provided by default for every
-        //image query, so the loading can be automatically performed by the IMG TAG itself and
-        //then used by the canvas (with the permission for exporting) [See ChEBI or PDBe use case]
-        RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, url);
-        requestBuilder.setHeader("X-Requested-With", "XMLHttpRequest");
-        try {
-            requestBuilder.sendRequest(null, new RequestCallback() {
-                @Override
-                public void onResponseReceived(Request request, Response response) {
-                    Image image = new Image("data:image/png;base64," + response.getText());
-                    forceLoadImage(handler, image);
-                }
-
-                @Override
-                public void onError(Request request, Throwable throwable) {
-                    handler.onChemicalImageLoaded(NOT_FOUND);
-                }
-            });
-        } catch (RequestException e) {
-            handler.onChemicalImageLoaded(NOT_FOUND);
-        }
+        forceLoadImage(handler, url);
     }
 
     public void loadChEBI(final Handler handler, String identifier){
         String id = identifier.replaceAll("^CHEBI[-:_]?", "");
         String url = "http://www.ebi.ac.uk/chebi/displayImage.do?defaultImage=true&chebiId=" + id + "&dimensions=200&transbg=true";
-
-        final Image image = new Image(url);
-        image.setAltText(url);
-        //Next line is meant to avoid the "SecurityError" problem when exporting tainted canvases
-        image.getElement().setAttribute("crossOrigin", "anonymous");
-        forceLoadImage(handler, image);
+        forceLoadImage(handler, url);
     }
 
-    private void forceLoadImage(final Handler handler, final Image image){
+    private void forceLoadImage(final Handler handler, String url){
+        final Image image = new Image(url);
+        image.setAltText(url);
+        //Forces requesting for an Access-Control-Allow-Origin
+        image.getElement().setPropertyBoolean("crossorigin", true);
+        //Next line is meant to avoid the "SecurityError" problem when exporting tainted canvases
+        image.getElement().setAttribute("crossOrigin", "anonymous");
+
         image.addLoadHandler(new LoadHandler() {
             @Override
             public void onLoad(LoadEvent loadEvent) {
