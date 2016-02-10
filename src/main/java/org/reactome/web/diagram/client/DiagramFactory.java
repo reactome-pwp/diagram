f9df572ba812b467fd018a538a06f8f2279a0a92
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-02-10 13:53:52 +0000

    Minor change

--- src/main/java/org/reactome/web/diagram/client/DiagramFactory.java
+++ src/main/java/org/reactome/web/diagram/client/DiagramFactory.java
@@ -20,7 +20,7 @@ public abstract class DiagramFactory {
     //It has a value by default but it can be set to a different one so in every load
     //the "user preferred" interactors resource will be selected
     public static String INTERACTORS_INITIAL_RESOURCE = "static"; // -> null here means DO NOT LOAD interactors
-    public static String INTERACTORS_INTIAL_RESOURCE_NAME = "IntAct (Static)"; // --> it should be null if the one above is null
+    public static String INTERACTORS_INITIAL_RESOURCE_NAME = "IntAct (Static)"; // --> it should be null if the one above is null
 
     //Added for testing
     public static DiagramViewer createDiagramViewer() {
