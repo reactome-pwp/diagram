2f7f57fd4cd3cc7eb1992d7deb3a770c56fc79bc
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-02-10 14:11:33 +0000

    Rendering is changed so that highlighting of entities and links is also fading in and out

--- src/main/java/org/reactome/web/diagram/renderers/interactor/s050/LoopLinkRenderer050.java
+++ src/main/java/org/reactome/web/diagram/renderers/interactor/s050/LoopLinkRenderer050.java
@@ -8,12 +8,22 @@ import org.reactome.web.diagram.util.AdvancedContext2d;
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
+@SuppressWarnings("Duplicates")
 public class LoopLinkRenderer050 extends LoopLinkAbstractRenderer {
     @Override
     public void draw(AdvancedContext2d ctx, DiagramInteractor item, Double factor, Coordinate offset) {
         ctx.save();
         ctx.setGlobalAlpha((factor - 0.5) * 2);
         super.draw(ctx, item, factor, offset);
+        ctx.restore();
+    }
+
+    @Override
+    public void highlight(AdvancedContext2d ctx, DiagramInteractor item, Double factor, Coordinate offset) {
+        double alpha = ctx.getGlobalAlpha();
+        ctx.save();
+        ctx.setGlobalAlpha((factor - 0.5) * alpha * 2);
+        super.highlight(ctx, item, factor, offset);
         ctx.restore();
     }
 }
