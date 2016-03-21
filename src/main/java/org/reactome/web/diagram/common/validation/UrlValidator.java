d2a6354efdf014ebbbf2b591a2be03fa9cdef418
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-03-21 16:08:34 +0000

    Change in the regular expression to match URLs

--- src/main/java/org/reactome/web/diagram/common/validation/UrlValidator.java
+++ src/main/java/org/reactome/web/diagram/common/validation/UrlValidator.java
@@ -1,14 +1,18 @@
 package org.reactome.web.diagram.common.validation;
 
+import com.google.gwt.regexp.shared.RegExp;
+
 /**
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
  */
 @SuppressWarnings("Duplicates")
 public class UrlValidator extends AbstractValidator {
 
-    private static final String regex = "<^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]>";
+    private static final String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+    private static RegExp regExp;
 
     public UrlValidator() {
+        regExp = RegExp.compile(regex);
     }
 
     @Override
@@ -18,7 +22,7 @@ public class UrlValidator extends AbstractValidator {
         if(aux.isEmpty()) {
             rtn = false;
             setTooltip("URL cannot be empty. Please provide one.");
-        } else if(!aux.matches(regex)) {
+        } else if(regExp.exec(aux) == null) {
             rtn = false;
             setTooltip("URL is not valid.");
         }
