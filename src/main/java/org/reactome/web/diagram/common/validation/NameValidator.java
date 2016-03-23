515cdf93f20a6416bd3f4da994fe22d3cc45776f
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-03-23 09:20:14 +0000

    Check for spaces added

--- src/main/java/org/reactome/web/diagram/common/validation/NameValidator.java
+++ src/main/java/org/reactome/web/diagram/common/validation/NameValidator.java
@@ -1,5 +1,7 @@
 package org.reactome.web.diagram.common.validation;
 
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
 import org.reactome.web.diagram.data.interactors.custom.ResourcesManager;
 
 import java.util.List;
@@ -23,9 +25,12 @@ public class NameValidator extends AbstractValidator {
     public boolean validate(String input) {
         boolean rtn = true;
         String aux = input.trim();
-        if(aux.isEmpty()) {
+        if (aux.isEmpty()) {
             rtn = false;
             setTooltip("Name cannot be empty. Please provide one.");
+        } else if(containsSpaces(aux)) {
+            rtn = false;
+            setTooltip("Name cannot contain spaces. Please remove them and try again");
         } else if(nameExists(aux)) {
             rtn = false;
             setTooltip("The name '" + aux + "' is already used. Please select another one.");
@@ -39,6 +44,12 @@ public class NameValidator extends AbstractValidator {
             if(existingName.toLowerCase().equals(name.toLowerCase())) return true;
         }
         return false;
+    }
+
+    private boolean containsSpaces(String name) {
+        RegExp regExp = RegExp.compile("\\s");
+        MatchResult matcher = regExp.exec(name);
+        return matcher != null;
     }
 
 }
