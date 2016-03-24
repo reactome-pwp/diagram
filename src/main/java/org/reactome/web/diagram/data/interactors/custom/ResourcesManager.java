bcb2e063b679782901721ba8c45001aa0a431d2c
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-03-24 14:06:00 +0000

    Using a map instead of a list to keep all resources

--- src/main/java/org/reactome/web/diagram/data/interactors/custom/ResourcesManager.java
+++ src/main/java/org/reactome/web/diagram/data/interactors/custom/ResourcesManager.java
@@ -8,103 +8,108 @@ import org.reactome.web.diagram.util.Console;
 import org.reactome.web.diagram.util.storage.StorageSolutionFactory;
 import org.reactome.web.diagram.util.storage.solutions.StorageSolution;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
+ * This class manages all custom resources and deals with choosing
+ * between HTML5 and cookie storage.
+ *
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
  */
 @SuppressWarnings("Duplicates")
 public class ResourcesManager {
+
+    static {
+        instance = new ResourcesManager();
+    }
+
     private static final String CUSTOM_RESOURCES_KEY = "Reactome.Diagram.CustomResources";
 
     private static ResourcesManager instance;
     private StorageSolution storageSolution;
 
-    private List<CustomResource> resources;
+    private Map<String, CustomResource> resources;  // token -> custom resource
 
     private ResourcesManager() {
         storageSolution = StorageSolutionFactory.getStorage();
         this.resources = loadItems(CUSTOM_RESOURCES_KEY);
     }
 
-    public static void initialise(){
-        if(instance != null) {
-            throw new RuntimeException("Resources Manager has already been initialised. Only one initialisation is permitted per Diagram Viewer instance.");
-        }
-        instance = new ResourcesManager();
-    }
-
-    public static ResourcesManager get(){
+    public static ResourcesManager get() {
         if (instance == null) {
             throw new RuntimeException("Resources Manager has not been initialised yet. Please call initialise before using 'get'");
         }
         return instance;
     }
 
-    public List<CustomResource> getResources() {
-        return resources;
+    public Collection<CustomResource> getResources() {
+        return resources.values();
+    }
+
+    public CustomResource getResource(String token) {
+        return resources.get(token);
     }
 
     public List<String> getResourceNames() {
         List<String> rtn = new LinkedList<>();
-        if(!resources.isEmpty()) {
-            for (CustomResource resource : resources) {
+        if (!resources.isEmpty()) {
+            for (CustomResource resource : resources.values()) {
                 rtn.add(resource.getName());
             }
+        }
+        Collections.sort(rtn);
+        return rtn;
+    }
+
+    public List<String> getResourceTokens() {
+        List<String> rtn;
+        if (resources.isEmpty()) {
+            rtn = new LinkedList<>();
+        } else {
+            rtn = new LinkedList<>(resources.keySet());
         }
         return rtn;
     }
 
-    /////////////////////////
-    // ==== Resources =====//
-    /////////////////////////
-
-    public void createAndAddResource(String name, String token){
+    public void createAndAddResource(String name, String token) {
         try {
             CustomResource cr = StoredResourcesModelFactory.create(CustomResource.class);
             cr.setName(name);
             cr.setToken(token);
 
-            resources.add(cr);
-            saveItems(CUSTOM_RESOURCES_KEY, resources);
+            resources.put(cr.getToken(), cr);
+            saveItems(CUSTOM_RESOURCES_KEY, resources.values());
         } catch (StoredResourcesModelException e) {
             e.printStackTrace();
         }
     }
 
     public void deleteResource(String token) {
-        CustomResource toBeDeleted = null;
-        for (CustomResource resource : resources) {
-            if(resource.getToken().equals(token)) {
-                toBeDeleted = resource;
-                break;
-            }
-        }
-        if(toBeDeleted!=null) {
-            resources.remove(toBeDeleted);
-            saveItems(CUSTOM_RESOURCES_KEY, resources);
+        CustomResource toBeDeleted = resources.remove(token);
+        if (toBeDeleted != null) {
+            saveItems(CUSTOM_RESOURCES_KEY, resources.values());
         }
     }
 
     public void deleteAllResources() {
-        resources = new LinkedList<>();
-        saveItems(CUSTOM_RESOURCES_KEY, resources);
+        resources = new HashMap<>();
+        saveItems(CUSTOM_RESOURCES_KEY, resources.values());
     }
 
     /***
      * Load all locally stored resources/tuples (if any)
      */
-    private List<CustomResource> loadItems(String key){
-        List<CustomResource> rtn = new LinkedList<>();
+    private Map<String, CustomResource> loadItems(String key) {
+        Map<String, CustomResource> rtn = new HashMap<>();
 
-        if(storageSolution!=null) {
+        if (storageSolution != null) {
             String json = storageSolution.read(key);
-            if(json != null) {
+            if (json != null) {
                 try {
                     CustomResources resources = StoredResourcesModelFactory.getModelObject(CustomResources.class, json);
-                    rtn.addAll(resources.getCustomResources());
-                    Console.info("Loaded " + rtn.size() + " custom items.");
+                    for (CustomResource customResource : resources.getCustomResources()) {
+                        rtn.put(customResource.getToken(), customResource);
+                    }
                 } catch (StoredResourcesModelException e) {
                     //TODO propagate the exception upwards
                     Console.info("Failed to load locally stored custom items");
@@ -118,11 +123,11 @@ public class ResourcesManager {
     /***
      * Persist resources/tuples to local storage
      */
-    private void saveItems(String key, List<CustomResource> resources) {
-        if(storageSolution!=null) {
+    private void saveItems(String key, Collection<CustomResource> resources) {
+        if (storageSolution != null) {
             try {
                 CustomResources container = StoredResourcesModelFactory.create(CustomResources.class);
-                container.setCustomResources(resources);
+                container.setCustomResources(new LinkedList<>(resources));
 
                 String json = StoredResourcesModelFactory.serialiseModelObject(container);
                 storageSolution.write(key, json);
