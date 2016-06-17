55ce7bc7e3774e214e19e41c132a25deefee13b5
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-06-17 17:08:57 +0100

    Replaced with lambda functions

--- src/main/java/org/reactome/web/diagram/search/infopanel/InfoActionsHelper.java
+++ src/main/java/org/reactome/web/diagram/search/infopanel/InfoActionsHelper.java
@@ -1,6 +1,8 @@
 package org.reactome.web.diagram.search.infopanel;
 
-import com.google.gwt.event.dom.client.*;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.event.shared.EventBus;
 import org.reactome.web.diagram.data.graph.model.GraphObject;
 import org.reactome.web.diagram.events.GraphObjectHoveredEvent;
@@ -13,30 +15,15 @@ import org.reactome.web.diagram.events.InteractorSelectedEvent;
 abstract class InfoActionsHelper {
 
     static ClickHandler getLinkClickHandler(final GraphObject reaction, final EventBus eventBus, final Object source){
-        return new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                eventBus.fireEventFromSource(new GraphObjectSelectedEvent(reaction, true), source);
-            }
-        };
+        return event -> eventBus.fireEventFromSource(new GraphObjectSelectedEvent(reaction, true), source);
     }
 
     static MouseOutHandler getLinkMouseOut(final EventBus eventBus, final Object source){
-        return new MouseOutHandler() {
-            @Override
-            public void onMouseOut(MouseOutEvent event) {
-                eventBus.fireEventFromSource(new GraphObjectHoveredEvent(), source);
-            }
-        };
+        return event -> eventBus.fireEventFromSource(new GraphObjectHoveredEvent(), source);
     }
 
     static MouseOverHandler getLinkMouseOver(final GraphObject reaction, final EventBus eventBus, final Object source){
-        return new MouseOverHandler() {
-            @Override
-            public void onMouseOver(MouseOverEvent event) {
-                eventBus.fireEventFromSource(new GraphObjectHoveredEvent(reaction), source);
-            }
-        };
+        return event -> eventBus.fireEventFromSource(new GraphObjectHoveredEvent(reaction), source);
     }
 
     /////////////////
@@ -44,22 +31,16 @@ abstract class InfoActionsHelper {
     /////////////////
 
     static ClickHandler getInteractorLinkClickHandler(final String url, final EventBus eventBus, final Object source){
-        return new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                event.stopPropagation(); event.preventDefault();
-                eventBus.fireEventFromSource(new InteractorSelectedEvent(url), source);
-            }
+        return event -> {
+            event.stopPropagation(); event.preventDefault();
+            eventBus.fireEventFromSource(new InteractorSelectedEvent(url), source);
         };
     }
 
     static ClickHandler getInteractionLinkClickHandler(final String url, final EventBus eventBus, final Object source){
-        return new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                event.stopPropagation(); event.preventDefault();
-                eventBus.fireEventFromSource(new InteractorSelectedEvent(url), source);
-            }
+        return event -> {
+            event.stopPropagation(); event.preventDefault();
+            eventBus.fireEventFromSource(new InteractorSelectedEvent(url), source);
         };
     }
 }
