71769c0341c6ac284c48ae27b6ff5f220332e5c6
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2016-07-29 11:19:15 +0100

    Button automatically collapses to its original status after the mouse moves out of it.

--- src/main/java/org/reactome/web/diagram/common/ConfirmationButton.java
+++ src/main/java/org/reactome/web/diagram/common/ConfirmationButton.java
@@ -2,36 +2,52 @@ package org.reactome.web.diagram.common;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.*;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.FocusPanel;
 
 
 /**
+ * A simple button that when clicked presents the user with two buttons to confirm the selected action.
+ * The button automatically collapses to its original status shortly after the mouse moves out of it.
+ *
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
  */
-public class ConfirmationButton extends Composite implements ClickHandler{
+public class ConfirmationButton extends Composite implements ClickHandler, MouseOverHandler, MouseOutHandler {
+    private static int WIDTH_COLLAPSED = 19;
+    private static int WIDTH_EXPANDED = 40;
+    private boolean isExpanded;
 
-    private FlowPanel container;
+    private FocusPanel container;
     private FlowPanel innerContainer;
     private IconButton normalButton;
     private IconButton okButton;
-    private IconButton cancelButton;
 
+    private IconButton cancelButton;
     private String title;
     private ImageResource icon;
-    private ClickHandler clickHandler;
 
+    private ClickHandler clickHandler;
+    private Timer collapseTimer;
 
     public ConfirmationButton(String title, ImageResource icon, ClickHandler clickHandler) {
         this.title = title;
         this.icon = icon;
         this.clickHandler = clickHandler;
+
+        collapseTimer = new Timer() {
+            @Override
+            public void run() {
+                collapse();
+            }
+        };
+
         init();
     }
 
@@ -42,6 +58,20 @@ public class ConfirmationButton extends Composite implements ClickHandler{
             expand();
         } else {
             collapse();
+        }
+    }
+
+    @Override
+    public void onMouseOut(MouseOutEvent event) {
+        if(isExpanded) {
+           collapseTimer.schedule(500);
+        }
+    }
+
+    @Override
+    public void onMouseOver(MouseOverEvent event) {
+        if(isExpanded && collapseTimer.isRunning()) {
+            collapseTimer.cancel();
         }
     }
 
@@ -67,24 +97,30 @@ public class ConfirmationButton extends Composite implements ClickHandler{
         innerContainer.add(okButton);
         innerContainer.add(cancelButton);
 
-        container = new FlowPanel();
+        container = new FocusPanel();
         container.setStyleName(RESOURCES.getCSS().container());
         container.add(innerContainer);
+        container.addMouseOutHandler(this);
+        container.addMouseOverHandler(this);
         this.initWidget(container);
 
         //Set it collapsed originally
         collapse();
     }
 
-    private void expand() {
-        container.getElement().getStyle().setWidth(40, Style.Unit.PX);
-        innerContainer.getElement().getStyle().setMarginLeft(-19, Style.Unit.PX);
+    private void collapse(){
+        container.getElement().getStyle().setWidth(WIDTH_COLLAPSED, Style.Unit.PX);
+        innerContainer.getElement().getStyle().setMarginLeft(0, Style.Unit.PX);
+        isExpanded = false;
     }
 
-    private void collapse(){
-        container.getElement().getStyle().setWidth(19, Style.Unit.PX);
-        innerContainer.getElement().getStyle().setMarginLeft(0, Style.Unit.PX);
+
+    private void expand() {
+        container.getElement().getStyle().setWidth(WIDTH_EXPANDED, Style.Unit.PX);
+        innerContainer.getElement().getStyle().setMarginLeft(-WIDTH_COLLAPSED, Style.Unit.PX);
+        isExpanded = true;
     }
+
 
     public static Resources RESOURCES;
     static {
