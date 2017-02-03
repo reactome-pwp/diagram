8b8e265fd074431dce51342fce2e0b5e6a1ed3c9
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2017-02-03 14:11:36 +0000

    Minor changes

--- src/main/java/org/reactome/web/diagram/common/fab/ExpandibleButton.java
+++ src/main/java/org/reactome/web/diagram/common/fab/ExpandibleButton.java
@@ -1,38 +1,34 @@
 package org.reactome.web.diagram.common.fab;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.Button;
 
 
 /**
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
  */
-public class ExpandibleButton extends AbsolutePanel {
+public class ExpandibleButton extends AbsolutePanel implements ClickHandler {
+    private EventBus eventBus;
 
-    public ExpandibleButton() {
-        super();
+    public ExpandibleButton(EventBus eventBus) {
+        this.eventBus = eventBus;
         setStyleName(RESOURCES.getCSS().container());
-
-        Button b1 = new Button();
-        b1.setStyleName(RESOURCES.getCSS().buttons());
-        Button b2 = new Button();
-        b2.setStyleName(RESOURCES.getCSS().buttons());
-        Button b3 = new Button();
-        b3.setStyleName(RESOURCES.getCSS().buttons());
-        Button b4 = new Button();
-        b4.setStyleName(RESOURCES.getCSS().buttons());
-
-        add(b1);
-        add(b2);
-        add(b3);
-        add(b4);
     }
 
-    public static Resources RESOURCES;
 
+    @Override
+    public void onClick(ClickEvent clickEvent) {
+
+    }
+
+
+
+    public static Resources RESOURCES;
     static {
         RESOURCES = GWT.create(Resources.class);
         RESOURCES.getCSS().ensureInjected();
@@ -52,7 +48,11 @@ public class ExpandibleButton extends AbsolutePanel {
 
         String container();
 
-        String buttons();
+        String baseButtons();
+
+        String optionButtons();
+
+        String first();
 
     }
 }
--- src/main/java/org/reactome/web/diagram/controls/top/RightTopLauncherPanel.java
+++ src/main/java/org/reactome/web/diagram/controls/top/RightTopLauncherPanel.java
@@ -9,7 +9,6 @@ import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.FlowPanel;
 import org.reactome.web.diagram.common.PwpButton;
-import org.reactome.web.diagram.common.fab.ExpandibleButton;
 import org.reactome.web.diagram.controls.top.common.AbstractMenuDialog;
 import org.reactome.web.diagram.controls.top.illustrations.DiagramIllustrations;
 import org.reactome.web.diagram.controls.top.key.DiagramKey;
@@ -64,7 +63,7 @@ public class RightTopLauncherPanel extends FlowPanel implements ClickHandler, Co
         this.exportBtn = new PwpButton("Export to pptx", RESOURCES.getCSS().export(), this);
         this.add(this.exportBtn);
 
-        add(new ExpandibleButton());
+        //add(new ExpandibleButton());
 
         this.diagramKeyBtn = new PwpButton("Diagram key", RESOURCES.getCSS().key(), this);
         this.add(this.diagramKeyBtn);
