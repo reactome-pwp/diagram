6fe2cb882c67030e09c25d4e0f25685ec10819d1
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2018-04-26 12:41:41 +0100

    Title panel added in the search details

--- src/main/java/org/reactome/web/diagram/search/detailspanel/DetailsInfoPanel.java
+++ src/main/java/org/reactome/web/diagram/search/detailspanel/DetailsInfoPanel.java
@@ -1,30 +1,43 @@
 package org.reactome.web.diagram.search.detailspanel;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 import org.reactome.web.diagram.data.Context;
-import org.reactome.web.diagram.data.graph.model.GraphObject;
+import org.reactome.web.diagram.data.graph.model.*;
+import org.reactome.web.diagram.data.interactors.model.InteractorSearchResult;
+import org.reactome.web.diagram.data.loader.LoaderManager;
 import org.reactome.web.diagram.events.ContentLoadedEvent;
 import org.reactome.web.diagram.events.ContentRequestedEvent;
-import org.reactome.web.diagram.events.GraphObjectSelectedEvent;
 import org.reactome.web.diagram.handlers.ContentLoadedHandler;
 import org.reactome.web.diagram.handlers.ContentRequestedHandler;
 import org.reactome.web.diagram.search.SearchArguments;
 import org.reactome.web.diagram.search.SearchPerformedEvent;
 import org.reactome.web.diagram.search.SearchPerformedHandler;
+import org.reactome.web.diagram.search.SearchResultObject;
 import org.reactome.web.diagram.search.events.AutoCompleteRequestedEvent;
+import org.reactome.web.diagram.search.events.PanelCollapsedEvent;
+import org.reactome.web.diagram.search.events.PanelExpandedEvent;
 import org.reactome.web.diagram.search.events.ResultSelectedEvent;
 import org.reactome.web.diagram.search.handlers.AutoCompleteRequestedHandler;
 import org.reactome.web.diagram.search.handlers.ResultSelectedHandler;
+import org.reactome.web.diagram.search.infopanel.DatabaseObjectListPanel;
+import org.reactome.web.diagram.search.infopanel.InteractorsListPanel;
 import org.reactome.web.diagram.search.panels.AbstractAccordionPanel;
-import org.reactome.web.diagram.search.results.InDiagramOccurrencesFactory;
+import org.reactome.web.diagram.search.results.ResultItem;
 import org.reactome.web.diagram.search.results.data.model.Occurrences;
+import org.reactome.web.diagram.search.results.local.InDiagramOccurrencesFactory;
 import org.reactome.web.diagram.util.Console;
+import org.reactome.web.diagram.util.MapSet;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
@@ -37,10 +50,26 @@ public class DetailsInfoPanel extends AbstractAccordionPanel implements ResultSe
     private EventBus eventBus;
     private Context context;
     private SearchArguments args;
+    private SearchResultObject selectedResultItem;
+
+    private FlowPanel mainPanel;
 
     public DetailsInfoPanel(EventBus eventBus) {
         this.eventBus = eventBus;
-        setStyleName(RESOURCES.getCSS().container());
+        show(false);
+
+        Label header = new Label("Details");
+        header.addStyleName(RESOURCES.getCSS().header());
+
+        FlowPanel main = new FlowPanel();
+        main.setStyleName(RESOURCES.getCSS().container());
+
+        mainPanel = new FlowPanel();
+        mainPanel.setStyleName(RESOURCES.getCSS().mainPanel());
+
+        main.add(header);
+        main.add(mainPanel);
+        add(main);
 
         eventBus.addHandler(ContentLoadedEvent.TYPE, this);
         eventBus.addHandler(ContentRequestedEvent.TYPE, this);
@@ -48,7 +77,8 @@ public class DetailsInfoPanel extends AbstractAccordionPanel implements ResultSe
 
     @Override
     public void onAutoCompleteRequested(AutoCompleteRequestedEvent event) {
-        //TODO Hide
+        selectedResultItem = null;
+        show(false);
     }
 
     @Override
@@ -59,27 +89,16 @@ public class DetailsInfoPanel extends AbstractAccordionPanel implements ResultSe
     @Override
     public void onContentRequested(ContentRequestedEvent event) {
         context = null;
+        Console.info("Set selected to null ");
+        selectedResultItem = null;
     }
 
     @Override
     public void onOccurrencesSuccess(Occurrences occurrences) {
-        this.clear();
         if (context == null)    return;
 
-        if (occurrences.getInDiagram()) {
-            //Selected entity is inside the diagram
-            this.add(new Label("In diagram"));
-//            GraphObject graphObject = context.getContent().getDatabaseObject(selectionModel.getSelectedObject().getStId());
-//            eventBus.fireEventFromSource(new GraphObjectSelectedEvent(graphObject, true), this);
-        }
-
-        List<String> occList = occurrences.getOccurrences();
-        if (occList != null && !occList.isEmpty()){
-            this.add(new Label("In Interacting Pathways"));
-            //Selected entity is inside an encapsulated "Interacting" pathway
-            GraphObject graphObject = context.getContent().getDatabaseObject(occList.get(0));
-            eventBus.fireEventFromSource(new GraphObjectSelectedEvent(graphObject, true), this);
-        }
+        populateWithOccurences(occurrences);
+        show(true);
     }
 
     @Override
@@ -89,16 +108,164 @@ public class DetailsInfoPanel extends AbstractAccordionPanel implements ResultSe
 
     @Override
     public void onResultSelected(ResultSelectedEvent event) {
-        Console.info( "Selection: " +  event.getSelectedResultItem().getStId() );
-        InDiagramOccurrencesFactory.searchForInstanceInDiagram(event.getSelectedResultItem().getStId(), args.getDiagramStId(), this);
+        selectedResultItem = event.getSelectedResultItem();
+
+        clearMainPanel();
+        includeWidget(new TitlePanel(selectedResultItem));
+
+        if(selectedResultItem instanceof ResultItem) {
+            InDiagramOccurrencesFactory.searchForInstanceInDiagram(((ResultItem) selectedResultItem).getStId(), args.getDiagramStId(), this);
+            Console.info("Details Panel: onResultSelected:" + ((ResultItem) selectedResultItem).getStId());
+        } else if (selectedResultItem instanceof InteractorSearchResult) {
+            populateWithInteractor();
+            show(true);
+        } else {
+            //Among others this covers the case of a null selected item
+            show(false);
+        }
+    }
+
+    @Override
+    public void onPanelCollapsed(PanelCollapsedEvent event) {
+        super.onPanelCollapsed(event);
+    }
+
+    @Override
+    public void onPanelExpanded(PanelExpandedEvent event) {
+        show(selectedResultItem!=null);
     }
 
     @Override
     public void onSearchPerformed(SearchPerformedEvent event) {
         args = event.getSearchArguments();
-        this.clear();
+        selectedResultItem = null;
+        show(false);
     }
 
+    private void populateWithOccurences(Occurrences occurrences) {
+        ResultItem selection = (ResultItem) selectedResultItem;
+        if (occurrences.getInDiagram()) {
+            //Selected entity is inside the diagram
+            GraphObject graphObject = context.getContent().getDatabaseObject(selection.getStId());
+        if(graphObject == null) { //TODO check this
+            Console.info(">>>> graphObject null" );
+        }
+            includeWidget(new DatabaseObjectListPanel("Directly in the diagram:", Collections.singletonList(graphObject), eventBus));
+
+            Collection<GraphReactionLikeEvent> participatesIn = new HashSet<>();
+            if(graphObject!=null && !graphObject.getDiagramObjects().isEmpty()){
+
+                if(graphObject instanceof GraphPhysicalEntity) {
+                    participatesIn = ((GraphPhysicalEntity) graphObject).participatesIn();
+                }else if(graphObject instanceof GraphReactionLikeEvent){
+                    //TODO encapsulate it into a method in ReactionLikeEvent
+                    Collection<GraphPhysicalEntity> rleParticipants = new HashSet<>();
+                    GraphReactionLikeEvent rle = (GraphReactionLikeEvent) graphObject;
+                    rleParticipants.addAll(rle.getInputs());
+                    rleParticipants.addAll(rle.getOutputs());
+                    rleParticipants.addAll(rle.getCatalysts());
+                    rleParticipants.addAll(rle.getActivators());
+                    rleParticipants.addAll(rle.getInhibitors());
+                    rleParticipants.addAll(rle.getRequirements());
+                    if (!rleParticipants.isEmpty()) {
+                        int size = rleParticipants.size();
+                        String title = "Participant" + (size>1?"s(":"(") + size + "):";
+                        mainPanel.add(new DatabaseObjectListPanel(title, rleParticipants, eventBus));
+                    }
+                }
+
+            }
+
+            if(graphObject instanceof GraphPhysicalEntity) {
+                GraphPhysicalEntity pe = (GraphPhysicalEntity) graphObject;
+                Set<GraphPhysicalEntity> parentLocations = pe.getParentLocations();
+                if (!parentLocations.isEmpty()) {
+                    Set<GraphComplex> complexes = new HashSet<>();
+                    Set<GraphEntitySet> sets = new HashSet<>();
+                    for (GraphPhysicalEntity parentLocation : parentLocations) {
+                        participatesIn.addAll(parentLocation.participatesIn());
+                        if (parentLocation instanceof GraphComplex) complexes.add((GraphComplex) parentLocation);
+                        else if (parentLocation instanceof GraphEntitySet) sets.add((GraphEntitySet) parentLocation);
+                    }
+
+                    if (!complexes.isEmpty()) {
+                        int size = complexes.size();
+                        String title = "Part of " + size + " complex" + (size > 1 ? "es:" : ":");
+                        mainPanel.add(new DatabaseObjectListPanel(title, complexes, eventBus));
+                    }
+
+                    if (!sets.isEmpty()) {
+                        int size = sets.size();
+                        String title = "Part of " + size + " set" + (size > 1 ? "s:" : ":");
+                        mainPanel.add(new DatabaseObjectListPanel(title, sets, eventBus));
+                    }
+                }
+            }
+
+            if(!participatesIn.isEmpty()){
+                int size = participatesIn.size();
+                String title = "Participates in " + size + " reaction" + (size>1?"s:":":");
+                mainPanel.add(new DatabaseObjectListPanel(title, participatesIn, eventBus));
+            }
+
+            // Include information about the interactors of this entity
+            if(context!=null && (graphObject instanceof GraphEntityWithAccessionedSequence || graphObject instanceof GraphSimpleEntity) ){
+                String resource = LoaderManager.INTERACTORS_RESOURCE.getName();
+                mainPanel.add(new InteractorsListPanel("According to " + resource + ", it interacts with:", context, (GraphPhysicalEntity) graphObject, eventBus));
+            }
+        }
+
+
+        List<String> occList = occurrences.getOccurrences();
+        if (occList != null && !occList.isEmpty()){
+            //Selected entity is inside an encapsulated "Interacting" pathway
+            Collection<GraphObject> list = occList.stream()
+                    .map(occ -> context.getContent().getDatabaseObject(occ))
+                    .collect(Collectors.toList());
+            mainPanel.add(new DatabaseObjectListPanel("Inside interacting pathway" + (list.size()>1 ? "s:" : ":"), list, eventBus));
+        }
+    }
+
+    private void populateWithInteractor() {
+        InteractorSearchResult selection = (InteractorSearchResult) selectedResultItem;
+
+        MapSet<Long, GraphObject> interactsWith = selection.getInteractsWith();
+        for(Long interactionId:interactsWith.keySet()) {
+            Set<GraphObject> interactors = new HashSet<>();
+            // Identify only those entities that are directly in the
+            // diagram and not part of a complex or a set
+            for (GraphObject graphObject : interactsWith.getElements(interactionId)) {
+                if(!graphObject.getDiagramObjects().isEmpty()){
+                    interactors.add(graphObject);
+                }
+            }
+            if (!interactors.isEmpty()) {
+                int interactorsSize = selection.getEvidences(interactionId) != null ? selection.getEvidences(interactionId) : 0;
+                String evidences = interactorsSize == 0 ? "" : (interactorsSize == 1 ? "(" + interactorsSize + " evidence)" : "(" + interactorsSize + " pieces of evidence)");
+
+                Double score = selection.getInteractionScore(interactionId);
+                String title = "Interacts with ";
+                title = title + "score: " + (score!=null ? NumberFormat.getFormat("0.000").format(score): "-") + " " + evidences;
+                mainPanel.add(new DatabaseObjectListPanel(title, interactors, eventBus));
+            }
+        }
+    }
+
+    private void includeWidget(Widget widget) {
+        mainPanel.add(widget);
+    }
+
+    private void clearMainPanel() {
+        mainPanel.clear();
+    }
+
+    private void show(boolean visible) {
+        if (visible) {
+            getElement().getStyle().setDisplay(Style.Display.INLINE);
+        } else {
+            getElement().getStyle().setDisplay(Style.Display.NONE);
+        }
+    }
 
     public static Resources RESOURCES;
     static {
@@ -113,8 +280,12 @@ public class DetailsInfoPanel extends AbstractAccordionPanel implements ResultSe
     @CssResource.ImportedWithPrefix("diagram-DetailsInfoPanel")
     public interface ResourceCSS extends CssResource {
 
-        String CSS = "org/reactome/web/diagram/search/infopanel/SelectionInfoPanel.css";
+        String CSS = "org/reactome/web/diagram/search/detailspanel/DetailsInfoPanel.css";
 
         String container();
+
+        String header();
+
+        String mainPanel();
     }
 }
---
+++ src/main/java/org/reactome/web/diagram/search/detailspanel/TitlePanel.java
@@ -0,0 +1,128 @@
+package org.reactome.web.diagram.search.detailspanel;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
+import org.reactome.web.diagram.data.interactors.model.InteractorSearchResult;
+import org.reactome.web.diagram.search.SearchResultObject;
+import org.reactome.web.diagram.search.results.ResultItem;
+
+/**
+ * Creates a title in the DetailsPanel containing various
+ * information about the selected item, such as name, accession,
+ * type, compartment etc.
+ *
+ * The view of the title is slightly different depending on whether
+ * the displayed item is a {@link ResultItem} or a {@link InteractorSearchResult}
+ *
+ * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
+ */
+public class TitlePanel extends FlowPanel {
+
+    private Label name;
+//    private Label type;
+
+    private FlowPanel firstLine;
+    private FlowPanel secondLine;
+
+    public TitlePanel(SearchResultObject selectedItem) {
+        initialise();
+
+        if (selectedItem instanceof ResultItem) {
+            populate((ResultItem) selectedItem);
+        } else if (selectedItem instanceof InteractorSearchResult) {
+            populate((InteractorSearchResult) selectedItem);
+        }
+    }
+
+    private void initialise() {
+        setStyleName(RESOURCES.getCSS().container());
+
+        name = new Label();
+        name.setStyleName(RESOURCES.getCSS().name());
+
+        firstLine = new FlowPanel();
+        firstLine.setStyleName(RESOURCES.getCSS().line());
+
+        secondLine = new FlowPanel();
+        secondLine.setStyleName(RESOURCES.getCSS().line());
+
+        add(name);
+        add(firstLine);
+        add(secondLine);
+    }
+
+    private void populate(ResultItem item) {
+        name.setText(item.getName());
+        name.setTitle(item.getName());
+
+        createAndAddLabel(item.getSchemaClass().name, "Type", RESOURCES.getCSS().type(), firstLine);
+        createAndAddLabel(item.getStId(), "Id", RESOURCES.getCSS().id(), firstLine);
+        createAndAddLabel("Accession", "Accession", RESOURCES.getCSS().accession(), firstLine);
+        createAndAddLabel(item.getCompartments(), "Compartments", RESOURCES.getCSS().compartments(), firstLine);
+        createAndAddLabel("Gene names", "Gene names", RESOURCES.getCSS().genes(), firstLine);
+    }
+
+    private void populate(InteractorSearchResult item) {
+        String alias = item.getAlias();
+        if (alias != null) {
+            name.setText(alias);
+            name.setTitle(alias);
+
+            createAndAddLabel(item.getAccession(), "Accession", RESOURCES.getCSS().accession(), firstLine);
+        } else {
+            name.setText(item.getAccession());
+            name.setTitle(item.getAccession());
+        }
+
+        //TODO check why the following is not working
+//        addLabelFor(item.getSchemaClass().name, "Type", RESOURCES.getCSS().type(), firstLine);
+        createAndAddLabel("Interactor", "Type", RESOURCES.getCSS().type(), firstLine);
+        createAndAddLabel(item.getResource().getName(), "Resource", RESOURCES.getCSS().id(), firstLine);
+    }
+
+    private void createAndAddLabel(String text, String tooltip, String style, Panel container) {
+        if(text != null && !text.isEmpty()) {
+            Label label = new Label();
+            label.setStyleName(style);
+            label.setText(text);
+            label.setTitle(tooltip);
+            container.add(label);
+        }
+    }
+
+    public static Resources RESOURCES;
+    static {
+        RESOURCES = GWT.create(Resources.class);
+        RESOURCES.getCSS().ensureInjected();
+    }
+    public interface Resources extends ClientBundle {
+        @Source(ResourceCSS.CSS)
+        ResourceCSS getCSS();
+    }
+
+    @CssResource.ImportedWithPrefix("diagram-TitlePanel")
+    public interface ResourceCSS extends CssResource {
+
+        String CSS = "org/reactome/web/diagram/search/detailspanel/TitlePanel.css";
+
+        String container();
+
+        String name();
+
+        String line();
+
+        String id();
+
+        String compartments();
+
+        String type();
+
+        String genes();
+
+        String accession();
+    }
+}
