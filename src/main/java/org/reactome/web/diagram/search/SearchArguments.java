7fa64e5e22929c4d897e2e05a6e0ca0bd4adf74e
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2018-05-02 11:28:20 +0100

    Escaping special characters before compiling the regular expression used for highlighting

--- src/main/java/org/reactome/web/diagram/search/SearchArguments.java
+++ src/main/java/org/reactome/web/diagram/search/SearchArguments.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
  * Immutable class that holds the arguments of the specific search.
  * e.g. the query, each of the search terms, the diagramId etc.
  * All query terms are stored in lowercase.
+ * <p/>
+ * Also this class is responsible for compiling once the regular expression
+ * used for highlighting of the results.
  *
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
@@ -18,33 +21,21 @@ public class SearchArguments {
     private String diagramStId;
     private String species;
     private Set<String> facets;
+    private int facetsScope;
 
     private List<String> terms = new ArrayList();
     private RegExp highlightingRegExp = null;
 
-    public SearchArguments(String query, String diagramStId, String species, Set<String> facets) {
+    public SearchArguments(String query, String diagramStId, String species, Set<String> facets, int facetsScope) {
         this.query = query.toLowerCase();
         this.diagramStId = diagramStId;
         this.species = species;
         this.facets = facets;
+        this.facetsScope = facetsScope;
 
         if (hasValidQuery()) {
             String[] allTerms = query.toLowerCase().split("  *");
-            if (allTerms.length != 0) {
-                terms = Arrays.asList(allTerms);
-
-                /*
-                 * (term1|term2)    : term is between "(" and ")" because we are creating a group, so this group can
-                 *                    be referred later.
-                 * gi               : global search and case insensitive
-                 * <b><u>$1</u></b> : instead of replacing by input, that would change the case, we replace it by $1,
-                 *                    that is the reference to the first matched group. This means that we want to
-                 *                    replace it using the exact word that was found.
-                 */
-                String aux = terms.stream()
-                                  .collect(Collectors.joining("|", "(", ")"));
-                highlightingRegExp = RegExp.compile(aux, "gi");
-            }
+            highlightingRegExp = compileHighlightingExpression(allTerms);
         }
     }
 
@@ -74,6 +65,10 @@ public class SearchArguments {
 
     public Set<String> getFacets() {
         return facets;
+    }
+
+    public int getFacetsScope() {
+        return facetsScope;
     }
 
     public RegExp getHighlightingExpression() {
@@ -106,5 +101,31 @@ public class SearchArguments {
                 ", species='" + species + '\'' +
                 ", facets='" + facets + '\'' +
                 '}';
+    }
+
+    private RegExp compileHighlightingExpression(String[] allTerms) {
+        RegExp highlightingRegExp = null;
+        if (allTerms.length != 0) {
+            terms = Arrays.asList(allTerms);
+
+            /*
+             * (term1|term2)    : term is between "(" and ")" because we are creating a group, so this group can
+             *                    be referred later.
+             * gi               : global search and case insensitive
+             * <b><u>$1</u></b> : instead of replacing by input, that would change the case, we replace it by $1,
+             *                    that is the reference to the first matched group. This means that we want to
+             *                    replace it using the exact word that was found.
+             */
+            String aux = terms.stream()
+                              .map(term -> RegExp.quote(term))
+                              .collect(Collectors.joining("|", "(", ")"));
+            try {
+                highlightingRegExp = RegExp.compile(aux, "gi");
+            } catch (RuntimeException e) {
+                //In case something goes wrong do not highlight anything
+                highlightingRegExp = RegExp.compile("/.^/");
+            }
+        }
+        return highlightingRegExp;
     }
 }
