f83da244d1bb103682bac6cc5d4452a00628c5e2
Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
2018-05-08 14:29:47 +0100

    Update the status of the button after handling the click event

--- src/main/java/org/reactome/web/diagram/common/IconToggleButton.java
+++ src/main/java/org/reactome/web/diagram/common/IconToggleButton.java
@@ -1,6 +1,11 @@
 package org.reactome.web.diagram.common;
 
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.resources.client.ImageResource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Kostas Sidiropoulos <ksidiro@ebi.ac.uk>
@@ -11,14 +16,26 @@ public class IconToggleButton extends IconButton {
     private ImageResource img;
     private ImageResource imgActive;
 
-    public IconToggleButton(String text, ImageResource img, ImageResource imgActive) {
+    private List<HandlerRegistration> clickHandlerRegistration = new ArrayList<>();
+
+    public IconToggleButton(String text, ImageResource img, ImageResource imgActive, ClickHandler handler) {
         super(text, img);
         this.img = img;
         this.imgActive = imgActive;
-        addClickHandler(event -> {
-            isActive = !isActive;
-            update();
-        });
+
+        setClickHandler(handler);
+    }
+
+    public void setClickHandler(ClickHandler handler) {
+        clickHandlerRegistration.forEach(HandlerRegistration::removeHandler);
+
+        clickHandlerRegistration.add(super.addClickHandler(handler));
+        clickHandlerRegistration.add(
+                addClickHandler(event -> {
+                    isActive = !isActive;
+                    update();
+                })
+        );
     }
 
     public boolean isActive() {
